64d1db10c2fad35fb03fb1b33b3fcd16f4f61cac
AnnaVitali <anna.vitali4@studio.unibo.it>
2022-12-23 19:23:47 +0100

    doc(Esp): add documentation to some files of the esp project

--- EspProject/MsgService.h
+++ EspProject/MsgService.h
@@ -3,27 +3,52 @@
 
 #include "Arduino.h"
 
+/**
+ * Deifnition of class Msg.
+*/
 class Msg
 {
   String content;
 
 public:
+  /**
+   * The public constructor.
+   * @param content, the content of the message.
+  */
   Msg(String content)
   {
     this->content = content;
   }
 
+/**
+ * Getter for obtaining the content of the message.
+ * @return the message content.
+*/
   String getContent()
   {
     return content;
   }
 };
 
+/**
+ * Interface MsgService.
+*/
 class MsgService
 {
   public:
+    /**
+     * Initialize the service.
+    */
     virtual void init();
+    /**
+     * Checks if a new message is available.
+     * @return true if there is an available message, flse otherwise.
+    */
     virtual bool isMsgAvailable();
+    /**
+     * Reads the available message.
+     * @return the message ridden.
+    */
     virtual Msg *receiveMsg();
 };
 #endif
--- EspProject/MsgServiceArduino.h
+++ EspProject/MsgServiceArduino.h
@@ -5,16 +5,38 @@
 #include "SoftwareSerial.h"
 #include "MsgService.h"
 
-
+/**
+ * Deifnition of class MsgServiceArduino that implements the MsgService interface.
+*/
 class MsgServiceArduino : public MsgService {
     
 public: 
+  /**
+   * The public constructor.
+   * @param rxPin, the rx pin connected to Arduino.
+   * @param txpin, the tx pin ocnnected to Arduino.
+  */
   MsgServiceArduino(int rxPin, int txPin);  
-  void init();  
+  /**
+   * Initialize the service.
+  */
+  void init(); 
+  /**
+   * Checks if a new message is available.
+   * @return true if there is an available message, flse otherwise.
+  */
   bool isMsgAvailable();
+  /**
+   * Reads the available message.
+   * @return the message ridden.
+  */
   Msg* receiveMsg();
+  /**
+   * Sends the message to Arduino.
+   * @param the message to send.
+  */
   void sendMsg(Msg msg);
-
+  
 private:
   String content;
   Msg* availableMsg;
