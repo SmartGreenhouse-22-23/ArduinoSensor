e8c8e184a6e1136b98e0d9d49aac58a1d48008bf
Elena Yan <elena.yan@studio.unibo.it>
2022-12-23 22:49:36 +0100

    doc: add documentation in Ventilation module

--- ArduinoProject/Fan.h
+++ ArduinoProject/Fan.h
@@ -4,19 +4,22 @@
 #include "Ventilation.h"
 #include "Arduino.h"
 
+/**
+ * Implementation of a Ventilation class representing a Fan device.
+*/
 class Fan: public Ventilation
 {
-public:
-    Fan(int enablePin, int diraPin, int dirbPin);
-    void activate();
-    void deactivate();
-    bool isActive();
+    public:
+        Fan(int enablePin, int diraPin, int dirbPin);
+        void activate();
+        void deactivate();
+        bool isActive();
 
-private:
-    int enablePin;
-    int diraPin;
-    int dirbPin;
-    bool active;
+    private:
+        int enablePin;
+        int diraPin;
+        int dirbPin;
+        bool active;
 };
 
 #endif
--- ArduinoProject/Ventilation.h
+++ ArduinoProject/Ventilation.h
@@ -1,11 +1,26 @@
 #ifndef __VENTILATION__
 #define __VENTILATION__
 
+/**
+ * Representation of a ventilation system.
+*/
 class Ventilation
 {
 public:
+    /**
+     * Activate the ventilation system.
+    */
     virtual void activate();
+
+    /**
+     * Deactivate the ventilation system.
+    */
     virtual void deactivate();
+
+    /**
+     * Check if the ventilation system is currently active.
+     * @return true if the ventilation system is active, false otherwise.
+    */
     virtual bool isActive();
 };
 
