c3b8777109fd313355be34ebb56804f537c950e7
Elena Yan <elena.yan@studio.unibo.it>
2022-12-27 18:38:31 +0100

    doc(esp): add documentation

--- EspProject/Esp8266.h
+++ EspProject/Esp8266.h
@@ -12,12 +12,23 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
+/**
+ * A class representing an ESP8266 WiFi module.
+*/
 class Esp8266: public Connection { 
     char* ssidName;
     char* pwd; 
     WiFiClient espClient;
     PubSubClient client;
     public:
+
+        /**
+         * Constructor of Esp8266 object.
+         * @param ssidName The SSID of the WiFi network.
+         * @param pwd The password of the WiFi network.
+         * @param mqttServer The address of the MQTT broker.
+         * @param msgARD A pointer to a MsgServiceArduino object.
+         */
         Esp8266(char *ssidName, char *pwd, char *mqttServer, MsgServiceArduino *msgARD);
         void connecting();
         void sendData(char* topic, String msg);
